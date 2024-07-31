ac92606d33bd3f3f65ff1d14b614e832cdc07a29
albin-git <albinsajan2001@gmail.com>
2024-07-31 14:29:57 +0530

    initial commit

---
+++ src/main/java/todoapp1/todoapp/Todoapp1Application.java
@@ -0,0 +1,13 @@
+package todoapp1.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todoapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Todoapp1Application.class, args);
+	}
+
+}
